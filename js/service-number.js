4e82812decefae0c10dc4a390eac73ff58acb56c
Mikhail Reevsky <woodwoold@gmail.com>
2018-04-11 14:24:27 +0300

    Do some

---
+++ js/service-number.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    var show = true;
+	var countbox = "#counts";
+	$(window).on("scroll load resize", function(){
+		if(!show) return false;                   // Отменяем показ анимации, если она уже была выполнена
+		var w_top = $(window).scrollTop();        // Количество пикселей на которое была прокручена страница
+		var e_top = $(countbox).offset().top;     // Расстояние от блока со счетчиками до верха всего документа
+		var w_height = $(window).height();        // Высота окна браузера
+		var d_height = $(document).height();      // Высота всего документа
+		var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+		if(w_top + 400 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+			$(".spincrement").spincrement({
+				thousandSeparator: "",
+				duration: 2000
+			});
+			show = false;
+		}
+	});
+});
